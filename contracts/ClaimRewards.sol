127a0e89d1317c7cd78a79f32de6ae9221742c91
ngduythao <ngduythao2805@gmail.com>
2022-08-30 09:03:40 +0700

    feat: no-contract calls

--- contracts/ClaimRewards.sol
+++ contracts/ClaimRewards.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
 
+import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 import { ReentrancyGuard } from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import { IClaimRewards } from "./interfaces/IClaimRewards.sol";
 import { Affiliate } from "./Affiliate.sol";
@@ -8,6 +9,8 @@ import { SignatureVerifier } from "./SignatureVerifier.sol";
 import { TokenRecover } from "./TokenRecover.sol";
 
 contract ClaimRewards is IClaimRewards, Affiliate, SignatureVerifier, TokenRecover, ReentrancyGuard {
+    using Address for address;
+
     mapping(bytes32 => bool) private _hasClaimed;
 
     receive() external payable {}
@@ -27,6 +30,7 @@ contract ClaimRewards is IClaimRewards, Affiliate, SignatureVerifier, TokenRecov
         address sender = _msgSender();
         bytes32 submissionId = keccak256(abi.encodePacked(claimId_, sender, tokens_, amounts_, deadline_));
 
+        if (sender != tx.origin || sender.isContract()) revert OnlyEOA();
         if (length != amounts_.length) revert LengthMismatch();
         if (_hasClaimed[claimId_]) revert DoubleSpending();
 
@@ -42,7 +46,7 @@ contract ClaimRewards is IClaimRewards, Affiliate, SignatureVerifier, TokenRecov
             }
         }
 
-        emit Claim(sender, submissionId);
+        emit Claim(sender, claimId_);
     }
 
     function hasClaim(bytes32 claimId_) external view returns (bool) {
--- contracts/interfaces/IClaimRewards.sol
+++ contracts/interfaces/IClaimRewards.sol
@@ -3,10 +3,11 @@ pragma solidity 0.8.16;
 
 interface IClaimRewards {
     /* ========== ERRORS ========== */
+    error OnlyEOA();
     error LengthMismatch();
     error DoubleSpending();
 
     /* ========== EVENTS ========== */
     /// @dev Emitted once the submission is confirmed by min required amount of oracles.
-    event Claim(address user, bytes32 submissionId);
+    event Claim(address user, bytes32 claimId);
 }
